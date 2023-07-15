d3f3a014c45db30b0b8b026f825065171d8b7912
Gurjit Singh <gurjit9320@gmail.com>
2023-07-15 22:08:28 +0530

    setting use client

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
-import Feed from "@components/Feed"
+"use client";
+
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
-            Discover & Share
-            <br className=""/>
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
-        </h1>
-        <p className="desc text-center">
-            PromptVerse is an open-source AI prompting tool for 
-            modern world to discover and share creative prompts
-        </p>
-        <Feed/>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptVerse is an open-source AI prompting tool for modern world to
+        discover and share creative prompts
+      </p>
+      <Feed />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
